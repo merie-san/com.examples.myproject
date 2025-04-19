ce07bb402dc9f564a48fe500d1bd0489823d39bb
merie-san <davide.zhang2@gmail.com>
2025-04-19 11:29:56 +0200

    initialization of Maven project for study in automated software testing

---
+++ myproject/src/test/java/com/examples/myproject/AppTest.java
@@ -0,0 +1,19 @@
+package com.examples.myproject;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class AppTest {
+
+	private App app;
+
+	@Before
+	public void setup() {
+		this.app = new App();
+	}
+
+	@Test
+	public void shouldAnswerWithTrue() {
+		assertEquals("Hello", app.sayHello());
+	}
+}
